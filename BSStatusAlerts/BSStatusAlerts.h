9caf62be6c068d7efae44325874a5ae6cf25deb9
William Savary <flip902@home.local>
2018-09-18 17:57:41 -0300

    Initial Commit

---
+++ BSStatusAlerts/BSStatusAlerts.h
@@ -0,0 +1,19 @@
+//
+//  BSStatusAlerts.h
+//  BSStatusAlerts
+//
+//  Created by William Savary on 2018-09-18.
+//  Copyright © 2018 William Savary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BSStatusAlerts.
+FOUNDATION_EXPORT double BSStatusAlertsVersionNumber;
+
+//! Project version string for BSStatusAlerts.
+FOUNDATION_EXPORT const unsigned char BSStatusAlertsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BSStatusAlerts/PublicHeader.h>
+
+
